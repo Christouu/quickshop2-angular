058851a2c1e1bd8f359247441d4ba06f588d7327
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-04-06 18:08:54 +0300

    finished Product page(needs more styles and html cuz its jsut meh)

--- quickShop/src/app/pages/product/product.component.ts
+++ quickShop/src/app/pages/product/product.component.ts
@@ -1,15 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface Product {
+  allKinds: boolean;
+  categories: string[];
+  description: string[];
+  image: string;
+  onSale: boolean;
+  price: number;
+  quantity: number;
+  title: string;
+  _id: string;
+}
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  id = '';
+  product: Product = {
+    allKinds: false,
+    categories: [],
+    description: [],
+    image: '',
+    onSale: false,
+    price: 0,
+    quantity: 0,
+    title: '',
+    _id: '',
+  };
 
-  constructor() { }
+  constructor(public http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http
+      .get<Product>(`http://localhost:5000/api/product/find/${id}`)
+      .subscribe((product) => {
+        this.product = product;
+      });
   }
-
 }
