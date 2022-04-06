0ac1df3f2dfa18d180e0276881a9d7d012b6efd7
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-04-06 11:03:22 +0300

    added verifyToken logic

---
+++ quickShopServer/middlewares/verifyToken.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Invalid token");
+
+      //save the user in req.user
+      req.user = user;
+
+      next();
+    });
+  } else {
+    res.status(401).json("You are not authenticated");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do this operation!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res
+        .status(403)
+        .json("You are not an Admin, you are not allowed to do this operation");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
