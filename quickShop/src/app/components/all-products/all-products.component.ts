11f8936c9293da38da230e4b285b755065e3a246
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-04-10 12:49:07 +0300

    added all products component to home page

---
+++ quickShop/src/app/components/all-products/all-products.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-products',
+  templateUrl: './all-products.component.html',
+  styleUrls: ['./all-products.component.scss'],
+})
+export class AllProductsComponent implements OnInit {
+  products: any = [];
+  allProductsStyles = true;
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:5000/api/product?new=true')
+      .subscribe((products) => {
+        this.products = products;
+      });
+  }
+}
